3b290f35cef779450dbb2b1b37b1b0d2c44ec15c
lsathish98 <45178403+lsathish98@users.noreply.github.com>
2018-11-20 23:43:17 +0530

    Add files via upload

---
+++ TestGit1.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class TestGit1 {
+	
+	public void methodOne() {
+		System.out.println("User lsathish98 Git Practice");
+
+	}
+	
+	public static void main(String[] args) {
+		TestGit1 t=new TestGit1();
+		t.methodOne();
+				
+		
+	}
+
+}
