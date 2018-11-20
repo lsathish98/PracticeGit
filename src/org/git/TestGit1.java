475fdfbff13f18c6a0d9e928be5846682ef882f7
lsathish98 <lsathish98@gmail.com>
2018-11-20 23:30:44 +0530

    Added user lsathish98 Program

---
+++ src/org/git/TestGit1.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class TestGit1 {
+	
+	public void methodOne() {
+		System.out.println("Git Practice");
+
+	}
+	
+	public static void main(String[] args) {
+		TestGit1 t=new TestGit1();
+		t.methodOne();
+				
+		
+	}
+
+}
