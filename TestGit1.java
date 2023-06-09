c7db04bf2f78bb13d521b0a1f35f94fcf6d628f3
sathi <sathi@LAPTOP-B1Q8V36Q>
2023-06-09 12:40:21 +0530

    commit from branch to master

--- TestGit1.java
+++ TestGit1.java
@@ -6,6 +6,8 @@ public class TestGit1 {
 		System.out.println("User lsathish98 Git Practice");
 		System.out.println("User lsathish98 Git Practice for branch");
 		System.out.println("User lsathish98 Git Practice for branch 2");
+		System.out.println("User lsathish98 Git Practice for branch 3");
+
 
 	}
 	
