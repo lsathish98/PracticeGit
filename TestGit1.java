09e4cdc428fdeb784ab6e5c0aab6fcdf950eab4c
sathi <sathi@LAPTOP-B1Q8V36Q>
2023-06-09 12:36:21 +0530

    push 2 from branch to master

--- TestGit1.java
+++ TestGit1.java
@@ -5,6 +5,7 @@ public class TestGit1 {
 	public void methodOne() {
 		System.out.println("User lsathish98 Git Practice");
 		System.out.println("User lsathish98 Git Practice for branch");
+		System.out.println("User lsathish98 Git Practice for branch 2");
 
 	}
 	
