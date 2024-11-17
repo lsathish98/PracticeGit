c08121160f917bf28e21c6928f549f3b6e4d9aff
lsathish98 <lsathish98@gmail.com>
2024-11-17 16:20:21 +0530

    Sathish - div

--- TestGit1.java
+++ TestGit1.java
@@ -10,14 +10,14 @@ public static int sub(int a, int b){
 	int val = a-b;
 	return val
 }
-public static int mul(int c, int d){
-		int val = c*d;
+public static int div(int c, int d){
+		int val = c/d;
 		return val
 }
 public static void main(String[] args) {
 		System.out.println(add(5,10));
 		System.out.println(sub(20,15));
-		System.out.println(mul(20,15));
+		System.out.println(div(20,15));
 	}
 
 }
